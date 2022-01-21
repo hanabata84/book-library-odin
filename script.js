10c4278076e64609a45906222e9cb011a5b4d159
hanabata84 <dianneerika.busa@gmail.com>
2022-01-21 16:07:09 +0900

    delete button added and other js functions

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const card = document.querySelector('.card');
 const para = document.createElement('p');
 const addBtn = document.querySelector('.add-btn');
 const modal = document.getElementById('modal')
+const closeBtn = document.querySelector('.close');
+const addBook = document.querySelector('.add-book');
+const mainCont = document.getElementById('main-container');
 
 const book = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
 
@@ -33,13 +36,49 @@ function addBookToLibrary() {
     const pagesNoTxt = document.getElementById('pagesNo').value;
     const readOrNotTxt = document.getElementById('readOrNot').checked;
     const newBook = new Book(titleTxt, authorTxt, pagesNoTxt, readOrNotTxt);
-    myLib.push(newBook);
-    para.textContent = '';
-    displayBooks();
+    myLib = newBook.displayObj().split(', ');
+    newElements();
+    closeModal();
 }
 
-function test(){
+function openModal(){
+    let inputs = document.getElementsByTagName('input');
+    for(let i = 0; i < inputs.length; i++){
+        inputs[i].value = '';
+        if(inputs[i].type == 'checkbox'){
+            inputs[i].checked = false;
+        }
+    }
     modal.style.display = 'block';
 }
 
-addBtn.addEventListener('click', test);
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+function newElements(){
+    const newCard = document.createElement('div');
+    const newPara = document.createElement('p');
+    const delBtn = document.createElement('button')
+    newCard.className = 'card';
+    mainCont.appendChild(newCard);
+    for(const lib of myLib){
+        newPara.textContent += lib + '\n';
+    }
+    newCard.appendChild(newPara);
+    delBtn.innerHTML = 'Delete Book';
+    delBtn.className = 'delete-book';
+    newCard.appendChild(delBtn);
+
+    delBtn.onclick = function(){
+        newCard.setAttribute('id','remove');
+        let delDiv = document.getElementById('remove');
+        delDiv.remove();
+    }
+}
+
+addBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+addBook.addEventListener('click', addBookToLibrary)
